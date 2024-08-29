68746559ae05f4699988c4aed03a4f93951e0457
fahadaleem <faleem396@gmail.com>
2024-08-29 21:46:15 +0500

    Create a github user activity cli project

---
+++ src/index.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+const [, , username] = process.argv;
+
+(async () => {
+  try {
+    const response = await fetch(
+      `https://api.github.com/users/${username}/events`
+    );
+    if (response.status === 200) {
+      const activities = await response.json();
+
+      if (!activities.length) {
+        console.log("No recent activity found!");
+      } else {
+        activities.forEach((activity) => {
+          if (activity.type === "PushEvent") {
+            console.log(
+              `Pushed ${activity.payload.commits.length} to ${activity.repo.name}`
+            );
+          } else if (activity.type === "CreateEvent") {
+            console.log(
+              `Created ${activity.payload.ref_type} in ${activity.repo.name}`
+            );
+          } else if (activity.type === "IssuesEvent") {
+            console.log(
+              `${
+                activity.payload.action.charAt(0).toUpperCase() +
+                activity.payload.action.slice(1)
+              } an issue in ${event.repo.name}`
+            );
+          }
+        });
+      }
+    } else {
+      const err = await response.json();
+      throw err;
+    }
+  } catch (err) {
+    console.log(err.message);
+  }
+})();
